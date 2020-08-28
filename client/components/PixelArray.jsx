d532a260d23cde4054dece3bfd2ca612c5463d4a
Marika Fiolitakis <marika.jf@gmail.com>
2020-08-28 13:29:43 +1200

    Add Pixel array

--- client/components/PixelArray.jsx
+++ client/components/PixelArray.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 import Pixel from './Pixel'
 
-class PixelArray extends React.Component {
-    render() {
-          const filledArray = Array(10000).fill(<Pixel />)
-           return filledArray
-    }
-  }
+const PixelArray = () => {          
+  return (
+    Array.from({length: 100}, (item, i) => <Pixel key={i}/>)   
+  )
+}
+
+// class PixelArray extends React.Component {
+//     render() {
+//           const filledArray = Array(10000).fill(<Pixel />)
+//            return filledArray
+//     }
+//   }
 
   export default PixelArray
